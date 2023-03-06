95ca2c40a72e818436f5b9b4999776fc10261046
Dalyunibaty <109936879+Dalyunibaty@users.noreply.github.com>
2023-03-06 02:57:12 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 # 设置各变量
 WSPATH=${WSPATH:-'argo'}
-UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
+UUID=${UUID:-'7f1eadba-9fdb-4a45-a6f3-a0dad7f0a85d'}
 
 generate_config() {
   cat > config.json << EOF
